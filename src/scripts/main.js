adcd0e6b3a71784a8229ef5d4e813f0f0fa6a5b7
Stanley <stanleybrenner@gmail.com>
2023-08-22 12:33:27 -0300

    First Commit :)

--- src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-addEventListener("DOMContentLoaded", function () {
-
-    document.getElementById("mainPageButton"), this.addEventListener("click", function () {
-        const meuH1 = document.getElementById('mainPageH1')
-        meuH1.innerHTML = "Você clicou no botão"
-    })
-})
