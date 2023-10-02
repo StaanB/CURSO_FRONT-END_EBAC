9e1cfaad430f53a79835d3d371c2b402b62da1fd
Stanley <stanleybrenner@gmail.com>
2023-10-02 15:38:03 -0300

    Commit for typescript exercise

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-const notaDaTurma = [{ "Nome": "João", "Nota": 7 }, { "Nome": "Fábio", "Nota": 8.5 }, { "Nome": "Maria", "Nota": 5 }, { "Nome": "Felipe", "Nota": 2 }, { "Nome": "Eduarda", "Nota": 9 }, { "Nome": "Marta", "Nota": 10 }]
-
-const mostrarAprovadosDaTurma = notaDaTurma.filter((aluno) => {
-    return aluno.Nota >= 6
-})
-
-console.log(mostrarAprovadosDaTurma);
+"use strict";
+function multiplicaDoisValores(a, b) {
+    console.log(a * b);
+}
+multiplicaDoisValores(7, 3);
+function saudacaoComNome(nome) {
+    console.log(`Olá ${nome}, tudo bom com você?`);
+}
+saudacaoComNome("Stanley");
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+function multiplicaDoisValores(a: number, b: number): void {
+    console.log(a * b);
+}
+
+multiplicaDoisValores(7, 3)
+
+function saudacaoComNome(nome: string): void {
+    console.log(`Olá ${nome}, tudo bom com você?`);
+}
+
+saudacaoComNome("Stanley")
